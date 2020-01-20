c034aed6026601de0489537535c3c9fd21b6c8f6
宮西洋輔 <miyanishi1212@gmail.com>
2020-01-21 00:40:05 +0900

    feat: NFCの読み込みまで実装

--- app/src/main/java/com/yosuke/nfcloader/MainActivity.kt
+++ app/src/main/java/com/yosuke/nfcloader/MainActivity.kt
@@ -1,33 +1,86 @@
 package com.yosuke.nfcloader
 
+import android.app.PendingIntent
+import android.content.Intent
+import android.nfc.NfcAdapter
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 import androidx.compose.Composable
+import androidx.compose.state
+import androidx.compose.unaryPlus
 import androidx.ui.core.Text
 import androidx.ui.core.setContent
+import androidx.ui.layout.Center
 import androidx.ui.material.MaterialTheme
 import androidx.ui.tooling.preview.Preview
+import java.util.*
 
+//TODO: アンチパターンまみれだと思うので要修正
 class MainActivity : AppCompatActivity() {
+    private val nfcAdapter by lazy {
+        NfcAdapter.getDefaultAdapter(this)
+    }
+    private val state by lazy { +state { "" } }
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContent {
             MaterialTheme {
-                Greeting("Android")
+                Center {
+                    ReadResult(state.value)
+                }
             }
+        }
+    }
+
+    override fun onResume() {
+        super.onResume()
+        // NFCがかざされたときの設定
+        val intent = Intent(this, MainActivity::class.java).apply {
+            flags = Intent.FLAG_ACTIVITY_SINGLE_TOP
+        }
+        // ほかのアプリを開かないようにする
+        val pendingIntent = PendingIntent.getActivity(
+                this,
+                0,
+                intent,
+                0
+        )
+        nfcAdapter.enableForegroundDispatch(
+                this,
+                pendingIntent,
+                null,
+                null
+        )
+    }
+
+    override fun onPause() {
+        nfcAdapter.disableForegroundDispatch(this)
+        super.onPause()
+    }
+
+    override fun onNewIntent(intent: Intent?) {
+        super.onNewIntent(intent)
+        if (intent == null) return
+        intent.getByteArrayExtra(NfcAdapter.EXTRA_ID).also {
+            state.value = Arrays.toString(it)
+            Log.d("read", state.value)
         }
     }
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun ReadResult(result: String) {
+    Text(text = "Read NFC -> $result")
 }
 
 @Preview
 @Composable
 fun DefaultPreview() {
     MaterialTheme {
-        Greeting("Android")
+        Center {
+            ReadResult("Android")
+        }
     }
 }
